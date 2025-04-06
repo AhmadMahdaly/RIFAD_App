acdc361c50e898449f1402d3c69c527761e08bc7
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-04-07 01:26:08 +0200

    Ensure Flutter bindings are initialized before setting preferred orientations

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/services.dart';
 import 'package:rifad/my_app.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Set the preferred orientations to portrait only
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
