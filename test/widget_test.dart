5f0709bfd2560b512a7b8e53a58033e7083aaad1
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-04-07 01:30:18 +0200

    Update import statement in widget_test.dart to reference my_app.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:rifad/main.dart';
+import 'package:rifad/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
