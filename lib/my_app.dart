aeb9e2f1bf961c6acc961bc1a02da3e53bdfb0ed
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-04-07 01:27:54 +0200

    Add flutter_screenutil dependency and update MyApp to use ScreenUtilInit

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'RIFAD', home: const Scaffold());
+    return const ScreenUtilInit(
+      designSize: Size(393, 852),
+      minTextAdapt: true,
+      child: MaterialApp(title: 'RIFAD', home: Scaffold()),
+    );
   }
 }
