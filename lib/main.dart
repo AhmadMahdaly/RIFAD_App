454ba3934899fd8d878c8a06895e486a587afb06
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-04-07 01:25:01 +0200

    Refactor app structure by moving MyApp class to a new file and updating main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:rifad/my_app.dart';
 
 void main() async {
   await SystemChrome.setPreferredOrientations([
@@ -8,13 +9,4 @@ void main() async {
   ]).then((_) {
     runApp(const MyApp());
   });
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(title: 'RIFAD', home: const Scaffold());
-  }
 }
