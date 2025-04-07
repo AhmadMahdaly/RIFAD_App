edfb01c418d5c68aae49d8dba97ab0feba9e6738
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-04-07 15:58:15 +0200

    Refactor SplashScreen to simplify layout by removing unnecessary Stack and Positioned widgets

--- lib/screens/splash_screen/splash_screen.dart
+++ lib/screens/splash_screen/splash_screen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:rifad/screens/login/login_screen.dart';
 import 'package:rifad/screens/splash_screen/widgets/splash_logo.dart';
-import 'package:rifad/utils/constants/colors_constants.dart';
+import 'package:rifad/widgets/end_of_page.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -29,56 +28,10 @@ class _SplashScreenState extends State<SplashScreen>
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        alignment: AlignmentDirectional.center,
-        children: [
-          const SplashLogoWidget(),
+    return const Scaffold(
+      body: SplashLogoWidget(),
 
-          Positioned(
-            bottom: 80.h,
-            right: -41.w,
-            child: ColorFiltered(
-              colorFilter: ColorFilter.mode(
-                Colors.deepPurple.withAlpha(75),
-                BlendMode.srcIn,
-              ),
-              child: Image.asset(
-                'assets/images/Rectangle.png',
-                width: 503.70.w,
-                height: 101.95.h,
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 10.h,
-            left: -41.w,
-            child: RotatedBox(
-              quarterTurns: 2,
-              child: ColorFiltered(
-                colorFilter: ColorFilter.mode(
-                  Colors.deepPurple.withAlpha(75),
-                  BlendMode.srcIn,
-                ),
-                child: Image.asset(
-                  'assets/images/Rectangle.png',
-                  width: 503.70.w,
-                  height: 101.95.h,
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 0.h,
-            child: Container(
-              height: 37.h,
-              width: MediaQuery.of(context).size.width,
-              color: kScaffoldBackgroundColor,
-            ),
-          ),
-        ],
-      ),
+      bottomNavigationBar: EndOfPage(),
     );
   }
 }
