8b220ec224f94e793e355e557c8a230d9ff97d4e
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-04-07 20:51:09 +0200

    Refactor CustomButton to improve active/inactive state handling and layout consistency

--- lib/utils/components/custom_button.dart
+++ lib/utils/components/custom_button.dart
@@ -5,43 +5,71 @@ import 'package:rifad/utils/constants/colors_constants.dart';
 class CustomButton extends StatelessWidget {
   const CustomButton({
     required this.text,
+    this.isActive = true,
     this.onTap,
     super.key,
     this.padding = 0,
   });
 
   final String text;
+  final bool isActive;
   final void Function()? onTap;
   final int padding;
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: onTap,
-      child: Container(
-        margin: EdgeInsets.symmetric(horizontal: padding.w),
-        alignment: Alignment.center,
-        width: double.infinity,
-        height: 62.h,
-        decoration: ShapeDecoration(
-          gradient: const LinearGradient(
-            begin: Alignment(1, 0.50),
-            end: Alignment(0, 0.50),
-            colors: [kSecondaryColor, kMainColor],
+    return isActive
+        ? InkWell(
+          onTap: onTap,
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: padding.w),
+            alignment: Alignment.center,
+            width: double.infinity,
+            height: 62.h,
+            decoration: ShapeDecoration(
+              gradient: const LinearGradient(
+                begin: Alignment(1, 0.50),
+                end: Alignment(0, 0.50),
+                colors: [kSecondaryColor, kMainColor],
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            child: Text(
+              text,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 21.sp,
+                fontWeight: FontWeight.w300,
+                height: 1.35.h,
+              ),
+            ),
           ),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
+        )
+        : InkWell(
+          onTap: onTap,
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: padding.w),
+            alignment: Alignment.center,
+            width: double.infinity,
+            height: 62.h,
+            decoration: ShapeDecoration(
+              color: kScaffoldBackgroundColor,
+              shape: RoundedRectangleBorder(
+                side: BorderSide(color: kTextColor, width: 1.w),
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            child: Text(
+              text,
+              style: TextStyle(
+                color: kTextColor,
+                fontSize: 21.sp,
+                fontWeight: FontWeight.w300,
+                height: 1.35.h,
+              ),
+            ),
           ),
-        ),
-        child: Text(
-          text,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 21.sp,
-            fontWeight: FontWeight.w300,
-            height: 1.35.h,
-          ),
-        ),
-      ),
-    );
+        );
   }
 }
