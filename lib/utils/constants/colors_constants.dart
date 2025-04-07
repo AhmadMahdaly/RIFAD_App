55efd438c8c2d82a5b107c964985ccfd548bece3
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-04-07 15:57:13 +0200

    Add color constants for improved theming and UI consistency

--- lib/utils/constants/colors_constants.dart
+++ lib/utils/constants/colors_constants.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 
 const kScaffoldBackgroundColor = Color(0xFFFEFEFE);
+const kMainColor = Color(0xFF6400CA);
+const kSecondaryColor = Color(0xFF3E017F);
+const kTextColor = Color(0xFF5D5E61);
