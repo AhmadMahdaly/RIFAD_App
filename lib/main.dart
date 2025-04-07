1f46235cb561c1cb130f4b96b43410c11b1e1b51
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-04-08 01:02:39 +0200

    Update dependencies in pubspec.yaml and main.dart for improved functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:rifad/my_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
-
+  await dotenv.load();
   // Set the preferred orientations to portrait only
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
