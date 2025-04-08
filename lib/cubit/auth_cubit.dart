a1d56d98a8cbd95c8b7bf65c2323520f43f89427
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-04-08 18:57:57 +0200

    Remove AppStates and AuthCubit classes to streamline state management

--- lib/cubit/auth_cubit.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:rifad/cubit/app_state.dart';
-
-class AuthCubit extends Cubit<AppStates> {
-  AuthCubit() : super(AppInitialState());
-}
