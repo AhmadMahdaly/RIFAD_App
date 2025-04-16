7fb6dee9efc8caeb4ace33c618598151f6c7021e
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-04-16 10:31:43 +0200

    Refactor & redesign Add station Page

--- lib/screens/services_pages/add_station/add_station_page.dart
+++ lib/screens/services_pages/add_station/add_station_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:rifad/core/utils/components/custom_button.dart';
-import 'package:rifad/core/utils/constants/colors_constants.dart';
 import 'package:rifad/screens/services_pages/add_station/widgets/add_station_body.dart';
 
 class AddStationPage extends StatelessWidget {
@@ -15,15 +14,15 @@ class AddStationPage extends StatelessWidget {
           icon: const Icon(Icons.arrow_back_ios_new),
           onPressed: () => Navigator.pop(context),
         ),
-        title: Text(
-          'إضافة',
-          style: TextStyle(
-            color: kMainColor,
-            fontSize: 18.sp,
-            fontWeight: FontWeight.w700,
-            height: 1.20.h,
-          ),
-        ),
+        // title: Text(
+        //   'إضافة',
+        //   style: TextStyle(
+        //     color: kMainColor,
+        //     fontSize: 18.sp,
+        //     fontWeight: FontWeight.w700,
+        //     height: 1.20.h,
+        //   ),
+        // ),
       ),
       body: const AddStationBody(),
       bottomNavigationBar: Padding(
--- lib/screens/services_pages/add_station/widgets/add_station_body.dart
+++ lib/screens/services_pages/add_station/widgets/add_station_body.dart
@@ -1,11 +1,145 @@
 import 'package:flutter/material.dart';
-import 'package:rifad/screens/services_pages/add_station/widgets/add_station_card.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:rifad/core/utils/components/custom_textformfield.dart';
+import 'package:rifad/core/utils/constants/colors_constants.dart';
 
 class AddStationBody extends StatelessWidget {
   const AddStationBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(children: [AddStationCard()]);
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          margin: EdgeInsets.only(top: 12.h, left: 16.w, right: 16.w),
+          height: 60.h,
+          padding: EdgeInsets.all(10.sp),
+          decoration: ShapeDecoration(
+            color: kMainColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(8.r),
+                topLeft: Radius.circular(8.r),
+              ),
+            ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'اسم المرحلة',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 13.sp,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Text(
+                'عدد الحجاج',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 13.sp,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Text(
+                'عدد الحافلات',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 13.sp,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Text(
+                'عدد الردود',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 13.sp,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            padding: EdgeInsets.all(16.sp),
+            itemCount: 2,
+            itemBuilder: (context, index) => const AddStationCard(),
+          ),
+        ),
+        // AddStationCard()
+      ],
+    );
+  }
+}
+
+class AddStationCard extends StatelessWidget {
+  const AddStationCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 8.h),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'مرحلة أولى',
+                style: TextStyle(
+                  color: kMainColor,
+                  fontSize: 14.sp,
+                  fontWeight: FontWeight.w500,
+                  height: 1.43.h,
+                ),
+              ),
+              SizedBox(
+                width: 80.w,
+                height: 38.h,
+                child: CustomTextformfield(
+                  textDirection: TextDirection.rtl,
+                  fontSize: 11,
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 8.w,
+                    vertical: 0,
+                  ),
+                  text: 'العدد',
+                ),
+              ),
+              SizedBox(
+                width: 80.w,
+                height: 38.h,
+                child: CustomTextformfield(
+                  textDirection: TextDirection.rtl,
+                  fontSize: 11,
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 8.w,
+                    vertical: 0,
+                  ),
+                  text: 'العدد',
+                ),
+              ),
+              SizedBox(
+                width: 80.w,
+                height: 38.h,
+                child: CustomTextformfield(
+                  textDirection: TextDirection.rtl,
+                  fontSize: 11,
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 8.w,
+                    vertical: 0,
+                  ),
+                  text: 'العدد',
+                ),
+              ),
+            ],
+          ),
+        ),
+        const Divider(color: kMainColorLightColor),
+      ],
+    );
   }
 }
