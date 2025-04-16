634ebf92dfea3ca0da781cd3cfffe5011bf27cb7
Ahmad Mahdaly <ahmed.mahdaly86@gmail.com>
2025-04-16 06:38:40 +0200

    Add new color constants for UI enhancements

--- lib/core/utils/constants/colors_constants.dart
+++ lib/core/utils/constants/colors_constants.dart
@@ -4,3 +4,10 @@ const kScaffoldBackgroundColor = Color(0xFFFEFEFE);
 const kMainColor = Color(0xFF6400CA);
 const kSecondaryColor = Color(0xFF3E017F);
 const kTextColor = Color(0xFF5D5E61);
+const kUnselectedIconColor = Color(0xffA2A2A2);
+const kMainColorLightColor = Color(0xFFEFE5F9);
+const kShadowColor = Color(0x0C000000);
+
+const kAnaDarkColor = Color(0xFF3E007E);
+const kAnaLight = Color(0xFF7E1BD0);
+const kAnaMedium = Color(0xFFA2A2A2);
